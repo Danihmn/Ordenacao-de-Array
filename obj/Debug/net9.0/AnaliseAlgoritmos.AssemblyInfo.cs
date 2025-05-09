b56e5bfcf24e845681f851a8d8d6810f32345f8d
Danihmn <daniel.bezerra.mult@gmail.com>
2025-05-08 22:56:29 -0300

    Pasta acidental excluída e novo arquivo README criado

--- obj/Debug/net9.0/AnaliseAlgoritmos.AssemblyInfo.cs
+++ obj/Debug/net9.0/AnaliseAlgoritmos.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("AnaliseAlgoritmos")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+fd11841dce585952bfceb497ec8a59c4a42b7a46")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a0b969f74c238b4b9ec6f24d47814d5073410ab2")]
 [assembly: System.Reflection.AssemblyProductAttribute("AnaliseAlgoritmos")]
 [assembly: System.Reflection.AssemblyTitleAttribute("AnaliseAlgoritmos")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
