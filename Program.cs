3f9c444667ca2471881c51645389f2161838c6bd
Danihmn <daniel.bezerra.mult@gmail.com>
2025-05-08 22:23:07 -0300

    Versão 1.0

---
+++ Program.cs
@@ -0,0 +1,33 @@
+﻿namespace AnaliseAlgoritmos
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] vetor = new int[6] { 3, 2, 6, 4, 7, 9 };
+            inSort(vetor);
+
+            foreach (int i in vetor)
+            {
+                Console.Write(i + " ");
+            }
+        }
+
+        static void inSort(int[] arr)
+        {
+            for (int i = 1; i < arr.Length; i++)
+            {
+                int chave = arr[i];
+                int j = i - 1;
+
+                while (j >= 0 && arr[j] > chave)
+                {
+                    arr[j + 1] = arr[j];
+                    j--;
+                }
+
+                arr[j + 1] = chave;
+            }
+        }
+    }
+}
