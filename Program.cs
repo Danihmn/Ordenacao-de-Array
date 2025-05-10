bd70fa68288b90b2df03d651245908dfda4af9c7
Danihmn <daniel.bezerra.mult@gmail.com>
2025-05-09 23:49:59 -0300

    Separando o código em responsabilidades

--- Program.cs
+++ Program.cs
@@ -5,10 +5,45 @@
         static void Main(string[] args)
         {
             int[] arr = new int[20] { 5, 2, 9, 1, 5, 6, 7, 3, 8, 4, 0, 10, 12, 11, 14, 13, 15, 17, 16, 18 };
-            int[] arr2 = new int[20] { 5, 2, 9, 1, 5, 6, 7, 3, 8, 4, 0, 10, 12, 11, 14, 13, 15, 17, 16, 18 };
+            int[] arr2 = new int[20] { 3, 2, 1, 5, 4, 0, 10, 12, 11, 14, 13, 15, 17, 16, 18, 19, 20, 21, 22, 23 };
 
+            Console.WriteLine("Antes da ordenação:");
+
+            Console.WriteLine("\n");
+
+            // Exibir os arrays antes da ordenação
+            Exibir(arr, arr2);
+
+            Ordenar(arr, arr2);
+
+            Console.WriteLine("\n\nDepois da ordenação:");
+
+            // Exibir os arrays após a ordenação
+            Console.WriteLine("\n");
+            Exibir(arr, arr2);
+        }
+
+        static void Ordenar(int[] arr, int[] arr2)
+        {
+            // Ordenar os arrays
             OrdenacaoAlgoritmos.BubbleSort(arr);
             OrdenacaoAlgoritmos.inSort(arr2);
+        }
+
+        static void Exibir(int[] arr, int[] arr2)
+        {
+            // Exibir os arrays ordenados
+            foreach (int i in arr)
+            {
+                Console.Write(i + " ");
+            }
+
+            Console.WriteLine("\n");
+
+            foreach (int i in arr2)
+            {
+                Console.Write(i + " ");
+            }
         }
     }
 
